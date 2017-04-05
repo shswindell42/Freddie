3c7bdb87785ed104a561a904aa08f515a1b7b61d
spencer.swindell <spencer.swindell@gmail.com>
2017-04-05 09:07:45 -0500

    Added web page to input features and return predicted interest rate

---
+++ Freddie.Web/InterestRateCalculator.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="InterestRateCalculator.aspx.cs" Inherits="Freddie.Web.InterestRateCalculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h1>Super Fancy Interest Calculator</h1>
+    <h6>It's fancy on the backend</h6>
+    <div>
+        Credit Score: <asp:TextBox runat="server" ID="tbCreditScore"></asp:TextBox> <br />
+        Loan to Value: <asp:TextBox runat="server" ID="tbLTV"></asp:TextBox> <br />
+        Monthly Income: <asp:TextBox runat="server" ID="tbIncome"></asp:TextBox> <br />
+        Monthly Debt Payments: <asp:TextBox runat="server" ID="tbDebt"></asp:TextBox> <br />
+        Loan Amount: <asp:TextBox runat="server" ID="tbAmount"></asp:TextBox> <br />
+
+        <asp:Button runat="server" ID="btnCalculate" Text="Calculate" OnClick="btnCalculate_Click" />
+        <br />
+        <br />
+        Calculated Interest Rate: <asp:Label runat="server" ID="lbInterestRate"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Freddie.Web/InterestRateCalculator.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using Microsoft.AnalysisServices.AdomdClient;
+
+namespace Freddie.Web
+{
+    public partial class InterestRateCalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // get the values from the form
+            string creditScore = tbCreditScore.Text;
+            string loanToValue = tbLTV.Text;
+            string monthlyIncome = tbIncome.Text;
+            string monthlyDebt = tbDebt.Text;
+            string loanAmount = tbAmount.Text;
+
+            // calculate dti
+            int dti = (int)((Convert.ToDouble(monthlyDebt) / Convert.ToDouble(monthlyIncome)) * 100);
+
+            // construct the query string
+            string dmx = @"SELECT PREDICT([Interest Rate]) 
+                            FROM [InterestRate LR]
+                            NATURAL PREDICTION JOIN
+                            (SELECT " + creditScore + @" as [Credit Score]
+                                , " + loanToValue + @"as [Original CTLV]
+                                , " + dti.ToString() + @" as [Original DTI Ratio]
+                                , " + loanAmount + @" as [Original UPB]
+                            ) as t";
+
+            // connect to ssas
+            AdomdConnection conn = new AdomdConnection(@"Data Source=localhost\MD;Catalog=Freddie SSAS");
+            conn.Open();
+
+            AdomdCommand cmd = new AdomdCommand(dmx, conn);
+            AdomdDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            double rate = (double)reader[0];
+
+            conn.Close();
+
+            lbInterestRate.Text = Math.Round(rate, 2).ToString();
+        }
+    }
+}
